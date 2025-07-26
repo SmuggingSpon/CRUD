07453de1b994b061ffec9cb0a7ec935b9e7360b8
ASUS <nguyenphunhan1110@gmail.com>
2025-07-26 18:58:26 +0700

    add JavaScript CRUD

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,49 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    let people = $state([
+        {first: "Cristiano", last: "Ronaldo"},
+        {first: "Lionel", last: "Messi"},
+        {first: "Harry", last: "Potter"}
+    ]);
+
+    let prefix = $state('');
+    let first = $state('');
+    let last = $state('');
+    let i = $state(0);
+
+    let filteredPeople = $derived(
+        prefix ? people.filter((person) => {
+            const name = `${person.last}, ${person.first}`;
+            return name.toLowerCase().startsWith(prefix.toLowerCase());
+        }) : people
+    );
+    let selected = $derived(filterPeople[i]);
+    
+    function create(){
+        people = people.concat({first, last});
+        i = people.length - 1;
+        first = last = '';
+    }
+
+    function update(){
+        selected.first = first;
+        selected.second = second;
+        people = people;
+    }
+
+    function remove(){
+        const index = people.indexOf(selected);
+        people = [...people.slice(0, index), ...people.slice(index + 1)];
+
+        first = last = '';
+        i = Math.min(i, filteredPeople.length - 2);
+    }
+
+    function reset_inputs(person){
+        first = person ? person.first : '';
+        last = person ? person.last : '';
+    }
+
+    $effect(() => {
+        reset_inputs(selected);
+    });
+</script>
